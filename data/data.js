7321fca5148bbd6da4b0c18be73a46652c080125
Matthieu F <fallourd.matthieu@gmail.com>
2019-04-14 18:37:44 +0200

    Github oauth

--- data/data.js
+++ data/data.js
@@ -1,3 +1,51 @@
 var express = require('express');
+var { default: ApolloClient, gql } = require('apollo-boost');
+var httpie = require('httpie');
 
+function getUserInfo(githubToken) {
+    return new Promise((resolve, reject) => {
+        const client = new ApolloClient({
+            uri: "https://api.github.com/graphql",
+            fetch: async (uri, options) => {
+                const { method } = options
+                options.family = 4
+                options.headers = {
+                    ...options.headers,
+                    'User-Agent': "github-insight"
+                }
+                const res = await httpie.send(method, uri, options)
+                return {
+                    text: async () => JSON.stringify(res.data),
+                    json: async () => res.data,
+                }
+            },
+            request: operation => {
+                operation.setContext({
+                    headers: {
+                        authorization: `Bearer ${githubToken}`,
+                    },
+                });
+            },
+        });
+
+        const GET_USER = gql`
+        {
+          viewer {
+            login
+            email
+          }
+        }
+        `;
+
+        client.query({
+            query: GET_USER,
+        })
+            .then(resolve).catch((error) => {
+            console.log(error);
+        });
+    })
+}
+
+
+module.exports.getUserInfo = getUserInfo;
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var process = require('process');
+var envConf = require('dotenv').config();
+var { getUserInfo } = require('../data/data');
+
+var githubOAuth = require('github-oauth')({
+  githubClient: process.env.GITHUB_KEY,
+  githubSecret: process.env.GITHUB_SECRET,
+  baseURL: process.env.adress,
+  loginURI: '/auth/github',
+  callbackURI: '/auth/github/callback'
+});
+
+function checkAuth(req,res,next) {
+  if (typeof req.cookies.token !== 'undefined') {
+    next();
+  } else {
+    res.redirect("/auth/github");
+  }
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let view = {
+    username: "",
+    email: ""
+  };
+  return getUserInfo(req.cookies.token).then((response) => {
+    view.username = response.data.viewer.login;
+    view.email = response.data.viewer.email;
+    res.render('index', { title: 'Express', elem: view });
+    return;
+  }).catch(() => {
+    res.render('index', { title: 'Express', elem: view });
+    return;
+  });
+});
+
+router.get('/auth/github', function(req,res) {
+  return githubOAuth.login(req,res);
+});
+
+router.get('/auth/github/callback', function(req,res) {
+  return githubOAuth.callback(req,res);
+});
+
+githubOAuth.on('error', function(err) {
+  console.error('there was a login error', err);
+});
+
+githubOAuth.on('token', function(token, serverResponse) {
+  console.log(token.access_token);
+  serverResponse.cookie('token', token.access_token);
+  serverResponse.redirect(`/`);
 });
 
 module.exports = router;
